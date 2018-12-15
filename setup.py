e743735e37b07582b892b48e59abb2b4ab9169e4
Lin Chen <linchen04@gmail.com>
2018-12-15 13:28:21 -0800

    Make quote consistent and add url
    Prior to this change,
    
    This change

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='py_grpc_prometheus',
-      version='0.1.0',
-      description='Python gRPC Prometheus Interceptors',
-      author='Lin Chen',
-      author_email='linchen04@gmail.com',
+setup(name="py_grpc_prometheus",
+      version="0.1.0",
+      description="Python gRPC Prometheus Interceptors",
+      author="Lin Chen",
+      author_email="linchen04@gmail.com",
       install_requires=[
-          'setuptools==39.0.1',
-          'grpcio>=1.10.0',
-          'prometheus_client==0.3.0'
+          "setuptools==39.0.1",
+          "grpcio>=1.10.0",
+          "prometheus_client==0.3.0"
       ],
-      url='',
+      url="https://github.com/lchenn/py-grpc-prometheus",
       packages=find_packages(exclude=["tests.*", "tests"]),
      )
--- tests/integration/hello_world/hello_world_client.py
+++ tests/integration/hello_world/hello_world_client.py
@@ -12,18 +12,18 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 _LOGGER = logging.getLogger(__name__)
 
 def call_server():
-  channel = grpc.intercept_channel(grpc.insecure_channel('localhost:50051'),
+  channel = grpc.intercept_channel(grpc.insecure_channel("localhost:50051"),
                                    PromClientInterceptor())
   stub = hello_world_grpc.GreeterStub(channel)
 
   # Call the unary-unary.
-  response = stub.SayHello(hello_world_pb2.HelloRequest(name='Unary'))
+  response = stub.SayHello(hello_world_pb2.HelloRequest(name="Unary"))
   _LOGGER.info("Unary response: %s", response.message)
   _LOGGER.info("")
 
   # Call the unary stream.
   _LOGGER.info("Running Unary Stream client")
-  response_iter = stub.SayHelloUnaryStream(hello_world_pb2.HelloRequest(name='unary stream'))
+  response_iter = stub.SayHelloUnaryStream(hello_world_pb2.HelloRequest(name="unary stream"))
   _LOGGER.info("Response for Unary Stream")
   for response in response_iter:
     _LOGGER.info("Unary Stream response item: %s", response.message)
@@ -45,7 +45,7 @@ def call_server():
 
 def generate_requests(name):
   for i in range(10):
-    yield hello_world_pb2.HelloRequest(name='%s %s' % (name, i))
+    yield hello_world_pb2.HelloRequest(name="%s %s" % (name, i))
 
 
 def run():
@@ -61,5 +61,5 @@ def run():
     quit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   run()
