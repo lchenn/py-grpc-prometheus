9019b8f9ee607922d0ed7d04453371fb59323182
Tiernan-Stapleton <tiernan.stapleton@hpe.com>
2020-11-16 09:57:46 +0000

    Implement enabling/disabling client histogram metrics in client_interceptor.

--- py_grpc_prometheus/prometheus_client_interceptor.py
+++ py_grpc_prometheus/prometheus_client_interceptor.py
@@ -5,11 +5,13 @@ from timeit import default_timer
 import grpc
 
 import py_grpc_prometheus.grpc_utils as grpc_utils
+from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STARTED_COUNTER
 from py_grpc_prometheus.client_metrics import GRPC_CLIENT_HANDLED_COUNTER
-from py_grpc_prometheus.client_metrics import GRPC_CLIENT_COMPLETED_LATENCY_SECONDS_HISTOGRAM
 from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STREAM_MSG_RECEIVED
 from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STREAM_MSG_SENT
-from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STARTED_COUNTER
+from py_grpc_prometheus.client_metrics import GRPC_CLIENT_HANDLED_HISTOGRAM
+from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STREAM_RECV_HISTOGRAM
+from py_grpc_prometheus.client_metrics import GRPC_CLIENT_STREAM_SEND_HISTOGRAM
 
 
 class PromClientInterceptor(grpc.UnaryUnaryClientInterceptor,
@@ -19,6 +21,12 @@ class PromClientInterceptor(grpc.UnaryUnaryClientInterceptor,
   """
   Intercept gRPC client requests.
   """
+
+  def __init__(self, enable_client_handling_time_histogram=False, enable_client_stream_receive_time_histogram=False, enable_client_stream_send_time_histogram=False):
+    self._enable_client_handling_time_histogram = enable_client_handling_time_histogram
+    self._enable_client_stream_receive_time_histogram = enable_client_stream_receive_time_histogram
+    self._enable_client_stream_send_time_histogram = enable_client_stream_send_time_histogram
+    super().__init__()
 
   def intercept_unary_unary(self, continuation, client_call_details, request):
     grpc_service_name, grpc_method_name, _ = grpc_utils.split_method_call(client_call_details)
@@ -32,10 +40,11 @@ class PromClientInterceptor(grpc.UnaryUnaryClientInterceptor,
     start = default_timer()
     handler = continuation(client_call_details, request)
 
-    GRPC_CLIENT_COMPLETED_LATENCY_SECONDS_HISTOGRAM.labels(
-        grpc_type=grpc_type,
-        grpc_service=grpc_service_name,
-        grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
+    if self._enable_client_handling_time_histogram:
+      GRPC_CLIENT_HANDLED_HISTOGRAM.labels(
+          grpc_type=grpc_type,
+          grpc_service=grpc_service_name,
+          grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
     GRPC_CLIENT_HANDLED_COUNTER.labels(
         grpc_type=grpc_type,
         grpc_service=grpc_service_name,
@@ -54,10 +63,11 @@ class PromClientInterceptor(grpc.UnaryUnaryClientInterceptor,
 
     start = default_timer()
     handler = continuation(client_call_details, request)
-    GRPC_CLIENT_COMPLETED_LATENCY_SECONDS_HISTOGRAM.labels(
-        grpc_type=grpc_type,
-        grpc_service=grpc_service_name,
-        grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
+    if self._enable_client_handling_time_histogram:
+      GRPC_CLIENT_HANDLED_HISTOGRAM.labels(
+          grpc_type=grpc_type,
+          grpc_service=grpc_service_name,
+          grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
 
     return grpc_utils.wrap_iterator_inc_counter(
         handler,
@@ -83,10 +93,11 @@ class PromClientInterceptor(grpc.UnaryUnaryClientInterceptor,
         grpc_type=grpc_type,
         grpc_service=grpc_service_name,
         grpc_method=grpc_method_name).inc()
-    GRPC_CLIENT_COMPLETED_LATENCY_SECONDS_HISTOGRAM.labels(
-        grpc_type=grpc_type,
-        grpc_service=grpc_service_name,
-        grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
+    if self._enable_client_handling_time_histogram:
+      GRPC_CLIENT_HANDLED_HISTOGRAM.labels(
+          grpc_type=grpc_type,
+          grpc_service=grpc_service_name,
+          grpc_method=grpc_method_name).observe(max(default_timer() - start, 0))
     return handler
 
   def intercept_stream_stream(self, continuation, client_call_details, request_iterator):
