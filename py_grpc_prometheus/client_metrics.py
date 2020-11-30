367f43db9930e5d28b38231c08de0d7bd39235d3
Tiernan-Stapleton <tiernan.stapleton@hpe.com>
2020-11-30 09:01:27 +0000

    Add legacy metrics to client_metrics.py

--- py_grpc_prometheus/client_metrics.py
+++ py_grpc_prometheus/client_metrics.py
@@ -37,3 +37,31 @@ GRPC_CLIENT_STREAM_SEND_HISTOGRAM = Histogram(
   "grpc_client_msg_send_handling_seconds",
   "Histogram of response latency (seconds) of the gRPC single message send.",
   ["grpc_type", "grpc_service", "grpc_method"])
+
+# Legacy metrics for backwards compatibility
+LEGACY_GRPC_CLIENT_STARTED_TOTAL_COUNTER = Counter(
+  "grpc_client_started_total",
+  "Total number of RPCs started on the client",
+  ["grpc_type", "grpc_service", "grpc_method"])
+
+LEGACY_GRPC_CLIENT_COMPLETED_COUNTER = Counter(
+  "grpc_client_completed",
+  "Total number of RPCs completed on the client, "
+  "regardless of success or failure.",
+  ["grpc_type", "grpc_service", "grpc_method", "code"])
+
+LEGACY_GRPC_CLIENT_COMPLETED_LATENCY_SECONDS_HISTOGRAM = Histogram(
+  "grpc_client_completed_latency_seconds",
+  "Histogram of rpc response latency (in seconds) for completed rpcs.",
+  ["grpc_type", "grpc_service", "grpc_method"])
+
+LEGACY_GRPC_CLIENT_MSG_RECEIVED_TOTAL_COUNTER = Counter(
+  "grpc_client_msg_received_total",
+  "Total number of stream messages received from the server.",
+  ["grpc_type", "grpc_service", "grpc_method"]
+)
+
+LEGACY_GRPC_CLIENT_MSG_SENT_TOTAL_COUNTER = Counter(
+  "grpc_client_msg_sent_total",
+  "Total number of stream messages sent by the client.",
+  ["grpc_type", "grpc_service", "grpc_method"])
